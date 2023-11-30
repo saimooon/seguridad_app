2482dd20eb898f01e889e7a047ea56d60f2fa304
Simon <simon@andaurservicios.cl>
2023-11-30 17:29:30 -0300

    Debug True

--- seguridad_app/settings.py
+++ seguridad_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-k#hms9w%)9o(_m#ny2#7v$30n5zp)stu!!(pzr7xevw3jjye6s')
 4
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
